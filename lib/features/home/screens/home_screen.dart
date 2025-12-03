5492cd3896038d74d3ede1c1dbf64b90397f6098
noyon-360 <nazibullahnoyon19.20@gmail.com>
2025-12-03 11:41:09 +0600

    grid layout update

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -99,7 +99,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 }
 
                 return GridLayout(
-                  mainAxisExtent: MediaQuery.of(context).size.height * 0.30, // smaller height
+                  // mainAxisExtent: MediaQuery.of(context).size.height * 0.30, // smaller height
                   itemCount: data.items.length,
                   itemBuilder: (_, index) {
                     final item = data.items[index];
--- lib/features/home/widgets/grid_layout.dart
+++ lib/features/home/widgets/grid_layout.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 
-
 class GridLayout extends StatelessWidget {
   const GridLayout({
     super.key,
@@ -13,20 +12,31 @@ class GridLayout extends StatelessWidget {
   final double? mainAxisExtent;
   final Widget? Function(BuildContext, int) itemBuilder;
 
+  int _getCrossAxisCount(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+
+    if (width >= 1200) return 5; // Large tablets / desktop
+    if (width >= 900) return 4; // Tablets in landscape
+    if (width >= 600) return 3; // Small tablets / large phones
+    return 2; // Default: mobile
+  }
+
   @override
   Widget build(BuildContext context) {
+    final crossAxisCount = _getCrossAxisCount(context);
     return GridView.builder(
-        itemCount: itemCount,
-        shrinkWrap: true,
-        padding: EdgeInsets.zero,
-        physics: NeverScrollableScrollPhysics(),
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          mainAxisExtent: mainAxisExtent,
-          mainAxisSpacing: 16,
-          crossAxisSpacing: 16,
-        ),
-        itemBuilder: itemBuilder
+      itemCount: itemCount,
+      shrinkWrap: true,
+      padding: EdgeInsets.zero,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: crossAxisCount,
+        mainAxisExtent: mainAxisExtent,
+        mainAxisSpacing: 16,
+        crossAxisSpacing: 16,
+        childAspectRatio: 0.75,
+      ),
+      itemBuilder: itemBuilder,
     );
   }
 }
